4181018cb3030dea712de19d530845a1cc56cafe
Ahmed Ayman <ahmed0ayman118@gmail.com>
2026-04-02 05:49:51 +0200

    stuff

--- Task_3/curve.cpp
+++ Task_3/curve.cpp
@@ -4,22 +4,37 @@
 
 using namespace std;
 
-const vector<vector<float>> H = { {1, 0, 0, 0},
+const vector<vector<int>> H = { {1, 0, 0, 0},
                 {0, 1, 0, 0},
                 {-3, -2, 3, -1},
                 {2, 1, -2, 1}};
 const int n = 1000;
 
 struct Point {
-    float x = 0.0, y = 0.0;
+    int x = 0, y = 0;
 };
 
-vector<vector<float>> multiply(const vector<vector<float>> &m1, const vector<vector<float>> &m2) {
+vector<vector<int>> multiply(const vector<vector<int>> &m1, const vector<vector<int>> &m2) {
     int r1 = m1.size();
     int c1 = m1[0].size();
     int c2 = m2[0].size();
 
-    vector<vector<float>> C(r1, vector<float>(c2, 0));
+    vector<vector<int>> C(r1, vector<int>(c2, 0));
+
+    for (int i = 0; i < r1; i++)
+        for (int j = 0; j < c2; j++)
+            for (int k = 0; k < c1; k++)
+                C[i][j] += m1[i][k] * m2[k][j];
+
+    return C;
+}
+
+vector<vector<double>> multiply(const vector<vector<double>> &m1, const vector<vector<int>> &m2) {
+    int r1 = m1.size();
+    int c1 = m1[0].size();
+    int c2 = m2[0].size();
+
+    vector<vector<double>> C(r1, vector<double>(c2, 0));
 
     for (int i = 0; i < r1; i++)
         for (int j = 0; j < c2; j++)
@@ -30,16 +45,16 @@ vector<vector<float>> multiply(const vector<vector<float>> &m1, const vector<vec
 }
 
 void hermite(HDC hdc, Point p1, Point T1, Point p2, Point T2) {
-    vector<vector<float>> G = {{p1.x, p1.y},
+    vector<vector<int>> G = {{p1.x, p1.y},
              {T1.x, T1.y}, // u1, v1
              {p2.x, p2.y},
              {T2.x, T2.y}}; // u2, v2
 
-    vector<vector<float>> C = multiply(H, G);
+    vector<vector<int>> C = multiply(H, G);
 
-    for(float t = 0; t < 1; t += 1.0f/n) {
-        vector<vector<float>> VT = {{1, t, t*t, t*t*t}};
-        vector<vector<float>> X = multiply(VT, C);
+    for(double t = 0; t < 1; t += 1.0f/n) {
+        vector<vector<double>> VT = {{1, t, t*t, t*t*t}};
+        vector<vector<double>> X = multiply(VT, C);
         SetPixel(hdc, round(X[0][0]), round(X[0][1]), RGB(255,0,0));
     }
 
