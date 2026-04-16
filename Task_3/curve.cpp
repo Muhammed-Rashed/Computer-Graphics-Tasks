cb4f40cebf74729f9d1ed7d2b947dc4120bc4f3c
Abdelrahman844 <Bodyakram7@gmail.com>
2026-04-16 10:06:55 +0200

    Edited tangent points

--- Task_3/curve.cpp
+++ Task_3/curve.cpp
@@ -8,7 +8,7 @@ const vector<vector<int>> H = { {1, 0, 0, 0},
                 {0, 1, 0, 0},
                 {-3, -2, 3, -1},
                 {2, 1, -2, 1}};
-const int n = 2000;
+const int n = 2500;
 
 struct Point {
     int x = 0, y = 0;
