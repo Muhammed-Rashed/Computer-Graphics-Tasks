bf071133246b7c39242e6c6835305194e15b13c3
Muhammed-Rashed <mohamed.rashedwork2005@gmail.com>
2026-05-11 14:17:09 +0300

    Ellipse

---
+++ Task_4/as-4.cpp
@@ -0,0 +1,175 @@
+#include <windows.h>
+#include <cmath>
+#include <ctime>
+using namespace std;
+
+#define IDM_ELLIPSE_DIRECT 1
+#define IDM_ELLIPSE_POLAR 2
+#define IDM_ELLIPSE_MIDPOINT 3
+
+void midpointEllipse(HDC hdc, int rx, int ry, int xc, int yc, COLORREF color)
+{
+    float dx, dy, d1, d2, x, y;
+    x = 0;
+    y = ry;
+
+    d1 = (ry * ry) - (rx * rx * ry) + (0.25 * rx * rx);
+    dx = 2 * ry * ry * x;
+    dy = 2 * rx * rx * y;
+
+    // Region 1
+    while (dx < dy)
+    {
+        SetPixel(hdc, x + xc, y + yc, color);
+        SetPixel(hdc, -x + xc, y + yc, color);
+        SetPixel(hdc, x + xc, -y + yc, color);
+        SetPixel(hdc, -x + xc, -y + yc, color);
+
+        if (d1 < 0)
+        {
+            x++;
+            dx = dx + (2 * ry * ry);
+            d1 = d1 + dx + (ry * ry);
+        }
+        else
+        {
+            x++;
+            y--;
+            dx = dx + (2 * ry * ry);
+            dy = dy - (2 * rx * rx);
+            d1 = d1 + dx - dy + (ry * ry);
+        }
+    }
+
+    d2 = ((ry * ry) * ((x + 0.5) * (x + 0.5))) + ((rx * rx) * ((y - 1) * (y - 1))) - (rx * rx * ry * ry);
+
+    // Region 2
+    while (y >= 0)
+    {
+        SetPixel(hdc, x + xc, y + yc, color);
+        SetPixel(hdc, -x + xc, y + yc, color);
+        SetPixel(hdc, x + xc, -y + yc, color);
+        SetPixel(hdc, -x + xc, -y + yc, color);
+
+        if (d2 > 0)
+        {
+            y--;
+            dy = dy - (2 * rx * rx);
+            d2 = d2 + (rx * rx) - dy;
+        }
+        else
+        {
+            y--;
+            x++;
+            dx = dx + (2 * ry * ry);
+            dy = dy - (2 * rx * rx);
+            d2 = d2 + dx - dy + (rx * rx);
+        }
+    }
+}
+
+LRESULT WINAPI WndProc(HWND hwnd, UINT mcode, WPARAM wp, LPARAM lp)
+{
+    static int clickCount = 0;
+    static POINT points[3];
+
+    switch (mcode)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+
+    case WM_LBUTTONDOWN:
+    {
+        points[clickCount].x = LOWORD(lp);
+        points[clickCount].y = HIWORD(lp);
+
+        clickCount++;
+
+        // After 3 clicks draw ellipse
+        if (clickCount == 3)
+        {
+            HDC hdc = GetDC(hwnd);
+
+            // Center
+            int xc = points[0].x;
+            int yc = points[0].y;
+
+            // rx
+            int rx = abs(points[1].x - xc);
+
+            // ry
+            int ry = abs(points[2].y - yc);
+
+            midpointEllipse(hdc, rx, ry, xc, yc, RGB(255, 0, 0));
+
+            ReleaseDC(hwnd, hdc);
+
+            clickCount = 0;
+        }
+
+        return 0;
+    }
+
+    case WM_COMMAND:
+    {
+        switch (LOWORD(wp))
+        {
+        case IDM_ELLIPSE_MIDPOINT:
+            clickCount = 0;
+            break;
+        }
+
+        return 0;
+    }
+    }
+
+    return DefWindowProc(hwnd, mcode, wp, lp);
+}
+
+int APIENTRY WinMain(HINSTANCE h, HINSTANCE p, LPSTR c, int nsh)
+{
+    srand((unsigned)time(NULL));
+    WNDCLASS wc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hbrBackground = (HBRUSH)GetStockObject(LTGRAY_BRUSH);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hIcon = LoadIcon(NULL, IDI_WINLOGO);
+    wc.hInstance = h;
+    wc.lpfnWndProc = WndProc;
+    wc.lpszClassName = "myclass";
+    wc.lpszMenuName = NULL;
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    RegisterClass(&wc);
+    HWND hwnd = CreateWindow("myclass", "Hello World",
+                             WS_OVERLAPPEDWINDOW, 0, 0, 800, 600, NULL, NULL, h, 0);
+
+    // Create menu bar
+    HMENU hMenu = CreateMenu();
+
+    // Create submenu
+    HMENU hEllipseMenu = CreatePopupMenu();
+
+    // Add submenu to menu bar
+    AppendMenu(hMenu, MF_POPUP, (UINT_PTR)hEllipseMenu, "Ellipse");
+
+    // Attach menu to window
+    SetMenu(hwnd, hMenu);
+
+    // Add items to submenu
+    // -- Ellipse --
+    AppendMenu(hEllipseMenu, MF_STRING, IDM_ELLIPSE_DIRECT, "Direct");
+    AppendMenu(hEllipseMenu, MF_STRING, IDM_ELLIPSE_MIDPOINT, "Midpoint");
+    AppendMenu(hEllipseMenu, MF_STRING, IDM_ELLIPSE_POLAR, "Polar");
+
+    ShowWindow(hwnd, nsh);
+    UpdateWindow(hwnd);
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    return 0;
+}
